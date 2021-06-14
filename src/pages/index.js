9da387ede6a9ff078a70557ee7d016d2155c1772
kletkaman <ivanov71nikita@yandex.ru>
2021-06-14 13:11:09 +0300

    Basic Styling with CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,21 +7,17 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Zero text</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+         <h1>Learn to design and code React apps</h1>
+         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+         <p>
+         <Link to="/page-2/">Watch the video</Link> <br />
+         </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
